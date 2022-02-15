a147a7d85051b65b59b34b0764d1cdc4a30cc454
Benoit <lacroixbenoit78@yhoo.fr>
2022-02-15 22:55:58 +0100

    list person

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPersonComponent } from './components/add-person/add-person.component';
+import { PersonDetailsComponent } from './components/person-details/person-details.component';
+import { PersonListComponent } from './components/person-list/person-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPersonComponent,
+    PersonDetailsComponent,
+    PersonListComponent
   ],
   imports: [
     BrowserModule,
